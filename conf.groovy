639590359a3dff4f400a670ff67e37f0dff6ed61
James Carnegie <me@kipz.org>
2013-02-05 01:25:47 +0000

    Fixed key

--- conf.groovy
+++ conf.groovy
@@ -14,11 +14,11 @@ media2flickr{
     videoTypes = ["avi","wmv","mov","mpeg","mpg","3gp","m2ts","ogg","ogv"]
 
     auth{
-        key = "65bb099a6a779eee9a6e06a1bd942952"
-        secret = "13f28866d1b43644"
+        key = "22c82f59e97410276ed0580e8ba0e1e5"
+        secret = "24110182ba694b56"
         token = System.properties['user.home'] + "/.media2flickr.token"
     }
     collections = [
-        [ "dir" : System.properties['user.home'] + "/Pictures/"]
+        [ "dir" : "/media/seagatedisk/pictures/"]
     ]
 }
